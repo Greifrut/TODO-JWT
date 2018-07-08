fd37a32e2cf12165b3d080e88f556eeebc29383d
Greifrut <artur.bunko@gmail.com>
2018-07-08 18:14:24 +0300

    Route api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'refresh/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post   'refresh', controller: :refresh, action: :create
+  post   'signin',  controller: :signin,  action: :create
+  post   'signup',  controller: :signup,  action: :create
+  delete 'signin',  controller: :signin,  action: :destroy
+
+  resource :todos
 end
